d793e7e1d631678c4680ec97bfb6d1f71dc602fc
JrSantiago <jrsantiaggo@gmail.com>
2023-04-19 21:02:01 -0300

    gerando JWT

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const {Router} = require("express");
 const usersRouter = require("./users.routes");
 const notesRoutes = require("./notes.routes");
 const tagsRoutes = require("./tags.routes");
+const sessionsRouter = require("./sessions.routes");
 
 const routes = Router();
 
 routes.use("/users", usersRouter)
 routes.use("/notes", notesRoutes)
 routes.use("/tags", tagsRoutes)
+routes.use("/sessions", sessionsRouter)
 
 module.exports = routes;
